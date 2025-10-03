3a3680d0550ce0bc80c40da458589ac189f14175
Dimka122 <kassindima1@gmail.com>
2025-10-03 11:11:51 +0300

    interfaces

---
+++ HotelBooking/HotelBooking.Application/Interfaces/IBookingService.cs
@@ -0,0 +1,17 @@
+﻿using HotelBooking.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Application.Interfaces
+{
+    public interface IBookingService
+    {
+        Task<BookingDto> CreateBookingAsync(string userId, int roomId, DateTime checkIn, DateTime checkOut);
+        Task<IEnumerable<BookingDto>> GetUserBookingsAsync(string userId);
+        Task<IEnumerable<BookingDto>> GetAllBookingsAsync();
+        Task CancelBookingAsync(int bookingId);
+    }
+}
---
+++ HotelBooking/HotelBooking.Application/Interfaces/IHotelService.cs
@@ -0,0 +1,19 @@
+﻿using HotelBooking.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelBooking.Application.Interfaces
+{
+    public interface IHotelService
+    {
+        Task<IEnumerable<HotelDto>> GetHotelsAsync();
+        Task<HotelDto> GetHotelByIdAsync(int id);
+        Task<IEnumerable<RoomDto>> SearchRoomsAsync(string city, DateTime checkIn, DateTime checkOut, int guests);
+        Task CreateHotelAsync(HotelDto hotelDto);
+        Task UpdateHotelAsync(HotelDto hotelDto);
+        Task DeleteHotelAsync(int id);
+    }
+}
