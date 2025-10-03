28b086a4378668ada048372b86dbc2461b261b26
Dimka122 <kassindima1@gmail.com>
2025-10-03 11:28:07 +0300

    Controller

---
+++ HotelBooking/HotelBooking.Web/Controllers/HotelsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using HotelBooking.Application.Interfaces;
+using HotelBooking.Application.DTOs;
+using HotelBooking.Domain.Enums;
+
+namespace HotelBooking.Web.Controllers
+{
+    public class HotelsController : Controller
+    {
+        private readonly IHotelService _hotelService;
+
+        public HotelsController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var hotels = await _hotelService.GetHotelsAsync();
+            return View(hotels);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Search(string city, DateTime checkIn, DateTime checkOut, int guests)
+        {
+            var rooms = await _hotelService.SearchRoomsAsync(city, checkIn, checkOut, guests);
+            return View("SearchResults", rooms);
+        }
+
+        [Authorize(Roles = Roles.Administrator)]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = Roles.Administrator)]
+        public async Task<IActionResult> Create(HotelDto hotelDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _hotelService.CreateHotelAsync(hotelDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hotelDto);
+        }
+    }
+}
